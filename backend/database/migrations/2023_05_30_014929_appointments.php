2d62c724439ed80b6282f1b56d47ba493d3f32d7
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2023-06-03 23:19:51 +0700

    hữu thêm các folder và file trong các thư mục để xây dựng code mọi người có thể chỉnh sửa

--- backend/database/migrations/2023_05_30_014929_appointments.php
+++ backend/database/migrations/2023_05_30_014929_appointments.php
@@ -1,14 +1,9 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
         Schema::create('appointments', function (Blueprint $table) {
@@ -17,12 +12,10 @@ return new class extends Migration
             $table->unsignedBigInteger('apartment_id');
             $table->dateTime('appointment_date_time');
             $table->string('status');
-
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('apartment_id')->references('apartment_id')->on('apartments');
         });
     }
-
     public function down()
     {
         Schema::dropIfExists('appointments');
--- backend/database/migrations/2023_05_31_141931_users.php
+++ backend/database/migrations/2023_05_31_141931_users.php
@@ -17,15 +17,13 @@ return new class extends Migration
             $table->string('fullname');
             $table->string('email')->unique();
             $table->string('phone');
-            $table->unsignedBigInteger('address_id');
+            $table->string('address');
             $table->string('password');
             $table->date('birthday');
+            $table-> string('role');
             $table->timestamps();
-
-            $table->foreign('address_id')->references('address_id')->on('addresses');
         });
     }
-
     /**
      * Reverse the migrations.
      */
@@ -33,6 +31,5 @@ return new class extends Migration
     {
         //
         Schema::dropIfExists('users');
-
     }
 };
