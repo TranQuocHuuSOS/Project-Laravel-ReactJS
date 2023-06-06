78722ab48bf1eab1e1d49fbd60eefc21399fe0c3
TranQuocHuuSOS <0tranquochuu02003@gmail.com>
2023-06-06 10:03:03 +0700

    đẩy header

---
+++ Front_end/src/component/Pages/User/Form_oppointment.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form_oppointment extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Form_oppointment;
